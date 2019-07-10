9e86a5ec8497c6a00b02a1fe8e4c75d764747766
Sopan Sanap <sopansanap0507@gmail.com>
2019-07-10 16:13:52 +0530

    Initial Commit

---
+++ RSLTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSLTest
+//
+//  Created by Shirish Vispute on 10/07/19.
+//  Copyright © 2019 Shirish Vispute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
